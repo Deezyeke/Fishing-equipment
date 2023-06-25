d029c0b6e4cc2f9493c9be625b432eb877057d54
Poga <pozsonyigabor96@gmail.com>
2023-06-25 21:24:42 +0200

    Add basic spring project skeleton files

---
+++ src/main/java/com/codecool/Fishing/FishingEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.Fishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingEquipmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/Fishing/FishingEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.Fishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
